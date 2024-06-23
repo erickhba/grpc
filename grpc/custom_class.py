b661d87e07b49fd7d00f4b37ddbbd04613b3343e
Erick Albuquerque <erickhba@gmail.com>
2024-06-23 20:55:03 -0300

    remove pasta nao usada

--- grpc/custom_class.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-class FakeDataAPI():
-    def get_comments():
-        response = requests.get("https://jsonplaceholder.typicode.com/posts/")
-        return response.content
-    
-if __name__ == "__main__":
-    print(FakeDataAPI.get_comments())
